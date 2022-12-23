f67c41fc5c89cdf9a2f1d7272ff489c1a257ac88
mrhacker679 <93563893+mrhacker679@users.noreply.github.com>
2022-12-23 08:51:15 -0600

    Update server.tf

--- server.tf
+++ server.tf
@@ -39,31 +39,5 @@ resource "aws_instance" "nexus" {
 }
 
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_all"
-  description = "Allow ALL"
-#   vpc_id      = "default"
-
-  ingress {
-    description      = "ALl"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_all"
-  }
-}
 
 
