198b4672b39eda2666bca1f56ad21f7204716fc4
mrhacker679 <93563893+mrhacker679@users.noreply.github.com>
2023-04-05 22:19:23 -0500

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,12 +2,12 @@
 
 sudo apt update
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-    /etc/apt/sources.list.d/jenkins.list > /dev/null
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install fontconfig openjdk-11-jdk -y
