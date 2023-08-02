76e49c1157d7008954838dd69490a37ced4f7ffa
mrhacker679 <93563893+mrhacker679@users.noreply.github.com>
2023-08-02 17:06:00 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt udpate 
-sudo apt-get install ca-certificates curl gnupg -y
+sudo apt-get install ca-certificates curl gnupg apt-transport-https -y
 
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -18,3 +18,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 # Installing minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
