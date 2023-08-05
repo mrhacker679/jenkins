614145393544462be0991e21fdd48958562bb6b1
mrhacker679 <93563893+mrhacker679@users.noreply.github.com>
2023-08-04 22:08:56 -0500

    Create centos-installation.sh

---
+++ centos-installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo yum update
+#Installing Java, Maven & Jenkins
+sudo yum install yum-utils epel-release -y
+sudo yum install java maven wget -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
+sudo rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins -y
+
+# installing Docker
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin --allowerasing
